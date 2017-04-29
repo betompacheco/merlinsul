1c0284961dd50c69c02e9f2345b8fad07275523b
Humberto de Melo Pacheco <humberto.mello@gmail.com>
2017-04-29 12:19:13 -0300

    Formatação de código

--- MerlinWeb/src/main/java/com/merlin/beans/incluir/IncluirProprietario.java
+++ MerlinWeb/src/main/java/com/merlin/beans/incluir/IncluirProprietario.java
@@ -5,153 +5,153 @@ import com.merlin.data.managers.ProprietarioManager;
 
 public class IncluirProprietario {
 
-    private ProprietarioDTO proprietario;
-    private int codigoproprietario;
-    private String nomeproprietario;
-    private String sexo;
-    private String identidade;
-    private String cpf;
-    private String profissao;
-    private String filiacao;
-    private String telResidencial;
-    private String telComercial;
-    private String telCelular;
-    private String mensagem;
+	private ProprietarioDTO proprietario;
+	private int codigoproprietario;
+	private String nomeproprietario;
+	private String sexo;
+	private String identidade;
+	private String cpf;
+	private String profissao;
+	private String filiacao;
+	private String telResidencial;
+	private String telComercial;
+	private String telCelular;
+	private String mensagem;
 
-    public void doIncluir() {
-        ProprietarioManager pm = new ProprietarioManager();
-        boolean novo = (codigoproprietario == 0);
-        if (novo) {
-            proprietario = pm.getNewBean();
-        } else {
-            proprietario = pm.select(codigoproprietario);
-            proprietario.setCodigoproprietario(new Integer(codigoproprietario));
-        }
-        proprietario.setNomeproprietario(nomeproprietario);
-        proprietario.setSexo(sexo);
-        proprietario.setIdentidade(identidade);
-        proprietario.setCpf(cpf);
-        proprietario.setProfissao(profissao);
-        proprietario.setFiliacao(filiacao);
-        proprietario.setTelResidencial(telResidencial);
-        proprietario.setTelComercial(telComercial);
-        proprietario.setTelCelular(telCelular);
-        if (novo) {
-            pm.insert(proprietario);
-            // limpa a tela
+	public void doIncluir() {
+		ProprietarioManager pm = new ProprietarioManager();
+		boolean novo = (codigoproprietario == 0);
+		if (novo) {
+			proprietario = pm.getNewBean();
+		} else {
+			proprietario = pm.select(codigoproprietario);
+			proprietario.setCodigoproprietario(new Integer(codigoproprietario));
+		}
+		proprietario.setNomeproprietario(nomeproprietario);
+		proprietario.setSexo(sexo);
+		proprietario.setIdentidade(identidade);
+		proprietario.setCpf(cpf);
+		proprietario.setProfissao(profissao);
+		proprietario.setFiliacao(filiacao);
+		proprietario.setTelResidencial(telResidencial);
+		proprietario.setTelComercial(telComercial);
+		proprietario.setTelCelular(telCelular);
+		if (novo) {
+			pm.insert(proprietario);
+			// limpa a tela
 
-            proprietario = null;
-            mensagem = "Proprietï¿½rio inserido com sucesso.";
-        } else {
-            pm.update(proprietario);
-            mensagem = "Proprietï¿½rio atualizado com sucesso.";
-        }
+			proprietario = null;
+			mensagem = "Proprietário inserido com sucesso.";
+		} else {
+			pm.update(proprietario);
+			mensagem = "Proprietário atualizado com sucesso.";
+		}
 
-    }
+	}
 
-    public int getCodigoproprietario() {
-        return codigoproprietario;
-    }
+	public int getCodigoproprietario() {
+		return codigoproprietario;
+	}
 
-    public void setCodigoproprietario(int codigoproprietario) {
-        this.codigoproprietario = codigoproprietario;
-    }
+	public String getCpf() {
+		return cpf;
+	}
 
-    public String getCpf() {
-        return cpf;
-    }
+	public String getFiliacao() {
+		return filiacao;
+	}
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
+	public String getIdentidade() {
+		return identidade;
+	}
 
-    public String getFiliacao() {
-        return filiacao;
-    }
+	public String getMensagem() {
+		return mensagem;
+	}
 
-    public void setFiliacao(String filiacao) {
-        this.filiacao = filiacao;
-    }
+	public String getNomeproprietario() {
+		return nomeproprietario;
+	}
 
-    public String getIdentidade() {
-        return identidade;
-    }
+	public String getProfissao() {
+		return profissao;
+	}
 
-    public void setIdentidade(String identidade) {
-        this.identidade = identidade;
-    }
+	public ProprietarioDTO getProprietario() {
+		return proprietario;
+	}
 
-    public String getMensagem() {
-        return mensagem;
-    }
+	public String getSexo() {
+		return sexo;
+	}
 
-    public void setMensagem(String mensagem) {
-        this.mensagem = mensagem;
-    }
+	public String getTelCelular() {
+		return telCelular;
+	}
 
-    public String getNomeproprietario() {
-        return nomeproprietario;
-    }
+	public String getTelComercial() {
+		return telComercial;
+	}
 
-    public void setNomeproprietario(String nomeproprietario) {
-        this.nomeproprietario = nomeproprietario;
-    }
+	public String getTelResidencial() {
+		return telResidencial;
+	}
 
-    public String getProfissao() {
-        return profissao;
-    }
+	public void setCodigoproprietario(int codigoproprietario) {
+		this.codigoproprietario = codigoproprietario;
+	}
 
-    public void setProfissao(String profissao) {
-        this.profissao = profissao;
-    }
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
 
-    public ProprietarioDTO getProprietario() {
-        return proprietario;
-    }
+	public void setFiliacao(String filiacao) {
+		this.filiacao = filiacao;
+	}
 
-    public void setProprietario(ProprietarioDTO proprietario) {
-        this.proprietario = proprietario;
-        this.codigoproprietario = proprietario.getCodigoproprietario().intValue();
-        this.nomeproprietario = proprietario.getNomeproprietario();
-        this.sexo = proprietario.getSexo();
-        this.identidade = proprietario.getIdentidade();
-        this.cpf = proprietario.getCpf();
-        this.profissao = proprietario.getProfissao();
-        this.filiacao = proprietario.getFiliacao();
-        this.telResidencial = proprietario.getTelResidencial();
-        this.telComercial = proprietario.getTelComercial();
-        this.telCelular = proprietario.getTelCelular();
-    }
+	public void setIdentidade(String identidade) {
+		this.identidade = identidade;
+	}
 
-    public String getSexo() {
-        return sexo;
-    }
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
 
-    public void setSexo(String sexo) {
-        this.sexo = sexo;
-    }
+	public void setNomeproprietario(String nomeproprietario) {
+		this.nomeproprietario = nomeproprietario;
+	}
 
-    public String getTelCelular() {
-        return telCelular;
-    }
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
+	}
 
-    public void setTelCelular(String telCelular) {
-        this.telCelular = telCelular;
-    }
+	public void setProprietario(ProprietarioDTO proprietario) {
+		this.proprietario = proprietario;
+		this.codigoproprietario = proprietario.getCodigoproprietario().intValue();
+		this.nomeproprietario = proprietario.getNomeproprietario();
+		this.sexo = proprietario.getSexo();
+		this.identidade = proprietario.getIdentidade();
+		this.cpf = proprietario.getCpf();
+		this.profissao = proprietario.getProfissao();
+		this.filiacao = proprietario.getFiliacao();
+		this.telResidencial = proprietario.getTelResidencial();
+		this.telComercial = proprietario.getTelComercial();
+		this.telCelular = proprietario.getTelCelular();
+	}
 
-    public String getTelComercial() {
-        return telComercial;
-    }
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
 
-    public void setTelComercial(String telComercial) {
-        this.telComercial = telComercial;
-    }
+	public void setTelCelular(String telCelular) {
+		this.telCelular = telCelular;
+	}
 
-    public String getTelResidencial() {
-        return telResidencial;
-    }
+	public void setTelComercial(String telComercial) {
+		this.telComercial = telComercial;
+	}
 
-    public void setTelResidencial(String telResidencial) {
-        this.telResidencial = telResidencial;
-    }
+	public void setTelResidencial(String telResidencial) {
+		this.telResidencial = telResidencial;
+	}
 }
--- MerlinWeb/src/main/java/com/merlin/data/DataBase.java
+++ MerlinWeb/src/main/java/com/merlin/data/DataBase.java
@@ -1,63 +1,71 @@
 package com.merlin.data;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DataBase {
 
-    public static String path = "";
+	public static String path = "";
 
-    public static Connection getConnection() {
-        Connection con = null;
-        try {
-            Class.forName("org.hsqldb.jdbcDriver");
-            // String url="jdbc:hsqldb:file:"+path+"banco/banco";
-            String url = "jdbc:hsqldb:hsql://localhost/banco";
-            String user = "sa";
-            String pass = "senha";
-            con = DriverManager.getConnection(url, user, pass);
+	public static Connection getConnection() {
+		Connection con = null;
+		try {
+			Class.forName("org.hsqldb.jdbcDriver");
+			// String url="jdbc:hsqldb:file:"+path+"banco/banco";
+			String url = "jdbc:hsqldb:hsql://localhost/banco";
+			String user = "sa";
+			String pass = "senha";
+			con = DriverManager.getConnection(url, user, pass);
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return con;
-    }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
 
-    /**
-     * @param filtros
-     * @param con
-     * @param qry
-     * @return
-     * @throws SQLException
-     */
-    public static PreparedStatement getStatementFiltered(List filtros, Connection con, String qry) throws SQLException {
-        String where = "";
-        List parametros = new ArrayList();
-        if (filtros != null) {
-            for (int i = 0; i < filtros.size(); i++) {
-                /*
-                 * Filter filtro = (Filter) filtros.get(i); Field campo = filtro.getField(); if (filtro.getValues().size()>0) { Object valor=filtro.getValues().get(0); if (campo.getType()==Field.TEXT){ if (valor!=null && valor.toString().trim().length()>0){ where+= (where.length()>0?" AND ":" ") +
-                 * campo.getName() + " like ? "; parametros.add(valor.toString() + "%"); } } }
-                 */
-            }
-        }
-        if (parametros.size() > 0) {
-            qry += " WHERE " + where;
-        }
-        PreparedStatement st = con.prepareStatement(qry);
+	/**
+	 * @param filtros
+	 * @param con
+	 * @param qry
+	 * @return
+	 * @throws SQLException
+	 */
+	public static PreparedStatement getStatementFiltered(List filtros, Connection con, String qry) throws SQLException {
+		String where = "";
+		List parametros = new ArrayList();
+		if (filtros != null) {
+			for (int i = 0; i < filtros.size(); i++) {
+				/*
+				 * Filter filtro = (Filter) filtros.get(i); Field campo =
+				 * filtro.getField(); if (filtro.getValues().size()>0) { Object
+				 * valor=filtro.getValues().get(0); if
+				 * (campo.getType()==Field.TEXT){ if (valor!=null &&
+				 * valor.toString().trim().length()>0){ where+=
+				 * (where.length()>0?" AND ":" ") + campo.getName() +
+				 * " like ? "; parametros.add(valor.toString() + "%"); } } }
+				 */
+			}
+		}
+		if (parametros.size() > 0) {
+			qry += " WHERE " + where;
+		}
+		PreparedStatement st = con.prepareStatement(qry);
 
-        if (parametros.size() > 0) {
-            for (int i = 0; i < parametros.size(); i++) {
-                Object valor = parametros.get(i);
-                if (valor instanceof String) {
-                    st.setString(i + 1, valor.toString());
-                }
-            }
+		if (parametros.size() > 0) {
+			for (int i = 0; i < parametros.size(); i++) {
+				Object valor = parametros.get(i);
+				if (valor instanceof String) {
+					st.setString(i + 1, valor.toString());
+				}
+			}
 
-        }
-        return st;
-    }
+		}
+		return st;
+	}
 }
